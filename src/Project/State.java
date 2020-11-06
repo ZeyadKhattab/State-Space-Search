ee1a95276ceebe1e843642a606b8e5fb63c5b8e5
Yahia Badr <yahya.hisham97@gmail.com>
2020-11-06 18:51:39 +0200

    Update State.java

--- src/Project/State.java
+++ src/Project/State.java
@@ -12,4 +12,51 @@ public class State {
 		ethan = this.ethan;
 		this.members = members;
 	}
+	
+	Character getEthan() {
+		return ethan;
+	}
+	
+	boolean moveUp() {
+		if(ethan.posY - 1 >= 0)
+		{
+			ethan.posY --;
+			return true;
+		}
+		return false;
+	}
+	boolean moveDown() {
+		if(ethan.posY + 1 <= gridH)
+		{
+			ethan.posY ++;
+			return true;
+		}
+		return false;
+	}
+	boolean moveLeft() {
+		if(ethan.posX - 1 >= 0)
+		{
+			ethan.posX --;
+			return true;
+		}
+		return false;
+	}
+	boolean moveRight() {
+		if(ethan.posX + 1 <= gridW)
+		{
+			ethan.posX ++;
+			return true;
+		}
+		return false;
+	}
+	int getRemainingMembers(){
+		return numberOfMembers - totalSaved;
+	}
+	
+	IMF[] getMembers() { return members;}
+	IMF getMember(int index) { return members[index];}
+	
+	int[][] getGrid(){
+		return grid;
+	}
 }
