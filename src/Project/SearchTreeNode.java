9d646a1a8ebbd849da1f6936507b81b6b11bc492
Zeyad Khattab <zeyad.khattab97@gmail.com>
2020-11-11 16:15:42 +0200

    Search Tree Node Constructor

--- src/Project/SearchTreeNode.java
+++ src/Project/SearchTreeNode.java
@@ -1,10 +1,21 @@
 package Project;
 
 public class SearchTreeNode {
+	State state;
+	int depth, cost;
+	SearchTreeNode parent;
+	Operator operator;
+
+	SearchTreeNode(SearchTreeNode parent, State state, Operator operator) {
+		// Cost assignment remaining
+		this.state = state;
+		this.parent = parent;
+		this.operator = operator;
+		depth = parent == null ? 0 : parent.depth + 1;
+	}
 
 }
 
 class Node {
-	
-}
 
+}
